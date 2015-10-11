0db9b173677f9ad37ab01621b79f6c36b010fb38
Christopher Allan Webber <cwebber@dustycloud.org>
2015-10-11 11:32:32 -0500

    Adding unit tests for deepcopy_jsobj

--- activipy/tests/test_types.py
+++ activipy/tests/test_types.py
@@ -17,6 +17,7 @@
 ##   See the License for the specific language governing permissions and
 ##   limitations under the License.
 
+import copy
 from activipy import types
 
 
@@ -90,3 +91,71 @@ def test_inheritance_list():
     assert ASOrderedCollectionPage.inheritance_chain == \
         [ASOrderedCollectionPage, ASOrderedCollection,
          ASCollection, ASObject, ASCollectionPage]
+
+
+ROOT_BEER_NOTE_JSOBJ = {
+    "@type": "Create",
+    "@id": "http://tsyesika.co.uk/act/foo-id-here/",
+    "actor": {
+        "@type": "Person",
+        "@id": "http://tsyesika.co.uk/",
+        "displayName": "Jessica Tallon"},
+    "to": ["acct:cwebber@identi.ca",
+           "acct:justaguy@rhiaro.co.uk"],
+    "object": {
+        "@type": "Note",
+        "@id": "htp://tsyesika.co.uk/chat/sup-yo/",
+        "content": "Up for some root beer floats?"}}
+
+ROOT_BEER_NOTE_MIXED_ASOBJ = {
+    "@type": "Create",
+    "@id": "http://tsyesika.co.uk/act/foo-id-here/",
+    "actor": types.ASObj({
+        "@type": "Person",
+        "@id": "http://tsyesika.co.uk/",
+        "displayName": "Jessica Tallon"}),
+    "to": ["acct:cwebber@identi.ca",
+           "acct:justaguy@rhiaro.co.uk"],
+    "object": types.ASObj({
+        "@type": "Note",
+        "@id": "htp://tsyesika.co.uk/chat/sup-yo/",
+        "content": "Up for some root beer floats?"})}
+
+
+def test_deepcopy_jsobj():
+    def looks_like_root_beer_note(jsobj):
+        return (
+            len(jsobj) == 5 and
+            jsobj["@type"] == "Create" and
+            jsobj["@id"] == "http://tsyesika.co.uk/act/foo-id-here/" and
+            isinstance(jsobj["actor"], dict) and
+            len(jsobj["actor"]) == 3 and
+            jsobj["actor"]["@type"] == "Person" and
+            jsobj["actor"]["@id"] == "http://tsyesika.co.uk/" and
+            jsobj["actor"]["displayName"] == "Jessica Tallon" and
+            isinstance(jsobj["to"], list) and
+            jsobj["to"] == ["acct:cwebber@identi.ca",
+                            "acct:justaguy@rhiaro.co.uk"] and
+            isinstance(jsobj["object"], dict) and
+            len(jsobj["object"]) == 3 and
+            jsobj["object"]["@type"] == "Note" and
+            jsobj["object"]["@id"] == "htp://tsyesika.co.uk/chat/sup-yo/" and
+            jsobj["object"]["content"] == "Up for some root beer floats?")
+
+    # We'll mutate later, so let's make a copy of this
+    root_beer_note = copy.deepcopy(ROOT_BEER_NOTE_JSOBJ)
+    assert looks_like_root_beer_note(root_beer_note)
+
+    # Test copying a compicated datastructure
+    copied_root_beer_note = types.deepcopy_jsobj(root_beer_note)
+    assert looks_like_root_beer_note(copied_root_beer_note)
+
+    # Mutate
+    root_beer_note["to"].append("sneaky@mcsneakers.example")
+    assert not looks_like_root_beer_note(root_beer_note)
+    # but things are still as they were in our copy right??
+    assert looks_like_root_beer_note(copied_root_beer_note)
+
+    # Test nested asobj copying
+    assert looks_like_root_beer_note(
+        types.deepcopy_jsobj(ROOT_BEER_NOTE_MIXED_ASOBJ))
